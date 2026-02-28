14d183a8ffb6c98bfb8a9338ca14b3234e8bc72f
Rabii <rabii.myself@gmail.com>
2026-02-28 17:50:05 +0000

    implemented firebase

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
-import AlarmScreen from './src/screens/AlarmScreen';
+import AlarmScreen from './src/screens/AlarmScreen.Js';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
